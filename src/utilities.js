5c9709a61d63fc63d324072db457dffb9ab28f2f
Sean Wilson <seancwilson@gmail.com>
2022-07-25 15:02:05 -0500

    update utilities, add `dasherize` & `markdown`

--- src/utilities.js
+++ src/utilities.js
@@ -1,3 +1,4 @@
+const MarkdownIt = require("markdown-it")
 const Sugar = require("sugar").extend()
 
 Sugar.Object.defineInstanceAndStatic({
@@ -20,36 +21,46 @@ function utilities(resume) {
 	return {
 		...resume,
 
-		has: function(key) {
+		has(key) {
 			return Object.has(resume, key)
 		},
 
-		get: function(key, defaultValue) {
+		get(key, defaultValue) {
 			const value = Object.get(resume, key)
 			return Object.isDefined(value) ? value : defaultValue
 		},
 
-		isEmpty: function(key) {
+		isEmpty(key) {
 			const value = Object.get(resume, key)
 			return Object.isEmpty(value)
 		},
 
-		join: function(keys, separator = ", ") {
+		join(keys, separator = ", ") {
 			return keys
 				.map(key => Object.get(resume, key))
 				.compactJoin(separator)
 		},
 
 		lowercase(str) {
-			return ((str == null) ? "" : str)
+			return (str ?? "")
 				.toString()
 				.toLowerCase()
+		},
+
+		dasherize(str) {
+			return (str ?? "")
+				.toString()
+				.dasherize()
 		},
 
 		normalizedPhone: resume?.basics?.phone?.replace(
 			/[^\d\+-]/g,
 			(match) => [".", "_", ")"].includes(match) ? "-" : ""
-		) ?? ""
+		) ?? "",
+
+		markdown: (md => function(str) {
+			return md.render(str ?? "")
+		})(new MarkdownIt())
 	}
 }
 
