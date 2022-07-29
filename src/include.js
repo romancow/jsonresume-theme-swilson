100925c99d1b11165545b892271c145cb03d0550
Sean Wilson <seancwilson@gmail.com>
2022-07-29 11:18:30 -0500

    set css vars on "DOMContentLoaded"

--- src/include.js
+++ src/include.js
@@ -1,6 +1,9 @@
 window.addEventListener('load', function() {
+	document.body.classList.add('loaded')
+})
+
+document.addEventListener('DOMContentLoaded', function() {
 	new URLSearchParams(window.location.search).forEach((value, key) => {
 		document.body.style.setProperty("--" + key, value)
 	})
-	document.body.classList.add('loaded')
 })
