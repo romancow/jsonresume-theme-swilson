6200c3e0c5e3a1a9030c92c5a85bd2ebc9de8235
Sean Wilson <seancwilson@gmail.com>
2022-07-28 19:11:30 -0500

    move stylus filter into index.js
    it's so small/basic, doesn't need it's own file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const pug = require("pug")
-const stylus = require("../src/stylus-pug-filter.js")
+const stylus = require('stylus')
 const utilities = require("../src/utilities.js")
+
+const filters = {
+	stylus(text, options) {
+		const stylusOptions = { compress: true, ...options }
+		return stylus.render(text, stylusOptions)
+	}
+}
 
 module.exports = {
 	render(resume) {
-		const options = { filters: { stylus }, ...utilities(resume) }
+		const options = { filters, ...utilities(resume) }
 		return pug.renderFile("index.pug", options)
 	}
 }
